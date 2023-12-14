04ebabd8d00b58bc46d3a0f27987509a81112ddb
ingelos <mailvaninge@gmail.com>
2023-12-14 18:56:27 +0100

    opdracht gedaan

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+
 import './App.css';
 import Whitening from './pages/whitening/Whitening.jsx';
 import Home from './pages/home/Home.jsx';
 import Appointments from './pages/appointments/Appointments.jsx';
 import Cavities from './pages/cavities/Cavities.jsx';
 import Navigation from './components/navigation/./Navigation.jsx';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
     return (
         <>
             <Navigation />
-            <Home />
-            <Whitening />
-            <Appointments />
-            <Cavities />
+            <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/tanden-bleken' element={<Whitening />}/>
+                <Route path='/afspraken' element={<Appointments />}/>
+                <Route path='/gaatjes' element={<Cavities />}/>
+            </Routes>
         </>
     );
 }
--- src/pages/cavities/Cavities.jsx
+++ src/pages/cavities/Cavities.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+
 import Header from '../../components/header/Header';
 import gaatjes from '../../assets/dentist.svg';
 import './Cavities.css';
+import {useNavigate} from "react-router-dom";
+
+
+
 
 function Cavities() {
-  return (
+
+    const navigate= useNavigate();
+
+    function handleClick() {
+        console.log('We gaan direct door naar de afspraken pagina!')
+        navigate('/afspraken')
+    }
+
+
+    return (
     <main className="page-container">
       <Header icon={gaatjes} title="Gaatjes" />
       <p>
@@ -12,7 +25,7 @@ function Cavities() {
         Amet consequuntur deleniti distinctio ea eveniet id, labore magni neque obcaecati praesentium quibusdam quidem, quod, repellat sequi ut.
         Blanditiis, reiciendis.
       </p>
-      <button type="button" className="appointment-button">
+      <button type="button" className="appointment-button" onClick={handleClick}>
         Maak direct uw afspraak!
       </button>
       <p className="content-container-col3">
